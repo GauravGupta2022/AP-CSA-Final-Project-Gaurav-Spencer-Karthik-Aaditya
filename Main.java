d63edb018929eabc4e122c8b5e961d02193ac0d2
SpencerFang2 <Sf1739@pleasantonusd.net>
2024-05-28 19:24:21 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
   public static void main(String[] args) {
       
       //CHANGE THIS LINE IF YOU WANT TO CHANGE THE TEXT FILE BEING READ AND THEREFORE THE MODE
-      final String textFileName = "GridSeating.txt"; 
+      final String textFileName = "GroupSeating.txt"; 
 
       FileReaderClass reader = new FileReaderClass(textFileName);
       Classroom classroom = reader.parse(); //complete classroom including arraylist of students
@@ -13,14 +13,15 @@ public class Main {
   
       System.out.println("Welcome to Seating Plus!");
       Scanner input = new Scanner(System.in);
-      System.out.println("Hello, would you like to get a completely randomized desk setup [1], let your students choose their own desks [2], or do you want to seat students by GPA [3]");
+      System.out.println("Hello, would you like to get a completely randomized desk setup [1] or to let your students choose their own desks [2]");
       String response = input.nextLine();
       
       if (response.contains("1")){ //completely randomized
         classroom.medicalFrontPreference();
-        System.out.println("e");
+        //System.out.println("e");
         classroom.medicalBackPreference();
         classroom.fullRandom();
+        System.out.println("If you would like to randomize it again, a button is provided");
       }
       
       else if (response.contains("2")) { //students choose own desks
@@ -29,10 +30,10 @@ public class Main {
         if (classroom.getUsingGridSeating()){
           //classroom.singleRandom(); <--- Debugging lines. NOT MEANT IN FINAL CODE!
           while (classroom.getStudentList().size()>1){
-            System.out.println("While Loop Execution");
+            //System.out.println("While Loop Execution");
             String s = classroom.singleRandom();
             classroom.personalPreference(s);
-            System.out.println(classroom.getStudentList().size());
+            //System.out.println(classroom.getStudentList().size());
           }
           // while (classroom.getStudentList().size()>0){
           //   classroom.singleRandom();
@@ -41,10 +42,11 @@ public class Main {
         }
         else {
           //code with group
+          // String groupResponse = input.next();
+          System.out.println("Press [1] to allow students to choose only one friend, press [2] to allow students to choose all friends");
           String groupResponse = input.next();
-          System.out.println("Press [1] to allow students to choose only one friend, press [2] to allow students to choose all friends");;
           if (groupResponse.equals("1")){
-            while (classroom.getStudentList().size()>0){
+            while (classroom.getStudentList().size()>1){
               classroom.personalPreference(classroom.singleRandom());
             }
           }
@@ -55,23 +57,23 @@ public class Main {
         }
       }
       
-      else if (response.contains("3")){ //seat by similar GPA (elite seating)
-        classroom.medicalFrontPreference();
-        classroom.medicalBackPreference();
-        //String holder="";
-        //while (classroom.getStudentList().size()>1){
-        //System.out.println("e");
-          //holder = classroom.singleRandom();
-          //classroom.gradePreference(holder);
-          System.out.println("f");
-          classroom.eliteSeating(50.0);
-          classroom.fullRandom();
-        }
+      // else if (response.contains("3")){ //seat by similar GPA (elite seating)
+      //   classroom.medicalFrontPreference();
+      //   classroom.medicalBackPreference();
+      //   //String holder="";
+      //   //while (classroom.getStudentList().size()>1){
+      //   //System.out.println("e");
+      //     //holder = classroom.singleRandom();
+      //     //classroom.gradePreference(holder);
+      //     System.out.println("f");
+      //     classroom.eliteSeating(50.0);
+      //     classroom.fullRandom();
+      //   }
       
       //  while (classroom.getStudentList().size()>0){
       //   classroom.fullRandom();
       //  }
-      System.out.println("indicator");
+      // System.out.println("indicator");
       ClassroomGUI gui = new ClassroomGUI(classroom, classroom.getUsingGridSeating());
       gui.initializeStudentInformation(classroom.getDesks());
       System.out.println("Thank you for using Seating Plus! We hope you enjoyed your experience!");
